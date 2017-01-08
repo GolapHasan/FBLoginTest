4760193e045b81ea65bdfd79672259c82423c9bf
orange <mhgolap11@oranges-Mac-mini.local>
2017-01-08 23:00:44 +0600

    initial commit

--- FBLoginTest/ViewController.swift
+++ FBLoginTest/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        loginButton.readPermissions = ["email"]
+        self.view.addSubview(loginButton)
+        
+        loginButton.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.logUserData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("logged in")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("logged out")
+    }
 
+    
+    func logUserData() {
+        let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest?.start(completionHandler: { (connection
+            , result, error) in
+            if(error != nil){
+                print(error)
+            }else{
+                print(result)
+            }
+        })
+    }
 
 }
 
